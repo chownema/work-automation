64586dbba66e6f37b8f36dc57573ee5a9a1ec512
Miguel Saavedra <Miguel Saavedra>
2017-04-26 10:38:21 +1200

    added more commands to get text from a file

--- _work.bat
+++ _work.bat
@@ -3,8 +3,17 @@ setlocal EnableDelayedExpansion
 wrongInput=0
 
 @echo off
+
+REM resource paths
+set resourceDir=C:\Users\chown\Documents\
+set angularFactoryTemplateLoc=Templates-Angular\TemplateFactoryAngular.js
+
+
+
+
 echo Hello Miguel what would you like to do?
 echo    1 Open LocalHost Webspace
+echo    2 Get Factory template
 REM echo    2 Open Work Space
 REM echo    3 Open Todos
 REM echo    4 Open LocalHost participant login to meeting
@@ -27,9 +36,16 @@ IF !command!==1 (
     IF !command!==2 (
         "webshortcuts/localhost-participant.url"
     )
+    exit /b
 ) 
-REM REM Work Space
-REM IF %command% == 2 
+REM Factory Template
+IF !command!==2 (
+    set template=%resourceDir%%angularFactoryTemplateLoc%
+    type !template! | clip 
+    echo Factory Template Copied into your clipboard
+    exit /b
+)
+
 REM REM Todos
 REM IF %command% == 3 
 REM REM localhost
@@ -47,3 +63,4 @@ REM Handle Errors
 IF %wrongInput%==1 (
     echo please input a number within the given options
 )
+
